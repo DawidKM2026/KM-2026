5d8e28e9e1570f50e75457040db39ff39ddb7835
Dawid N <harlchannels@gmail.com>
2026-07-07 09:32:21 +0200

    Dodanie OTA + poprawy strony

--- hello_world/main/hello_world_main.c
+++ hello_world/main/hello_world_main.c
@@ -4,7 +4,6 @@
  * SPDX-License-Identifier: CC0-1.0
  */
 
-#include <stdio.h>
 #include <inttypes.h>
 #include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
@@ -14,7 +13,6 @@
 #include "esp_system.h"
 #include <stdio.h>
 #include "esp_log.h"
-#include "esp_system.h"
 #include "esp_https_ota.h"
 #include "esp_http_client.h"
 #include "nvs_flash.h"
@@ -24,37 +22,41 @@
 #include "esp_netif.h"
 #include "esp_ota_ops.h"
 #include "esp_http_server.h"
+#include "esp_partition.h"
 
 //--------------------------------- Access Point ----------------------------------------
 
-#define WIFI_SSID      "KM Projekt v1.0.0"
-#define WIFI_PASS      "KM123456"
-#define WIFI_CHANNEL   1
-#define MAX_STA_CONN   4
+#define WIFI_SSID "KM Projekt v1.0.0"
+#define WIFI_PASS "KM123456"
+#define WIFI_CHANNEL 1
+#define MAX_STA_CONN 4
 
 static const char *TAG = "wifi_ap";
 
+// Informacje o wifi do debug'owania
 static void wifi_event_handler(
     void *arg,
     esp_event_base_t event_base,
     int32_t event_id,
-    void *event_data
-)
+    void *event_data)
 {
-    if (event_id == WIFI_EVENT_AP_STACONNECTED) {
+    if (event_id == WIFI_EVENT_AP_STACONNECTED)
+    {
         wifi_event_ap_staconnected_t *event =
-            (wifi_event_ap_staconnected_t *) event_data;
+            (wifi_event_ap_staconnected_t *)event_data;
 
         ESP_LOGI(TAG, "Device connected, AID=%d", event->aid);
-    } 
-    else if (event_id == WIFI_EVENT_AP_STADISCONNECTED) {
+    }
+    else if (event_id == WIFI_EVENT_AP_STADISCONNECTED)
+    {
         wifi_event_ap_stadisconnected_t *event =
-            (wifi_event_ap_stadisconnected_t *) event_data;
+            (wifi_event_ap_stadisconnected_t *)event_data;
 
         ESP_LOGI(TAG, "Device disconnected, AID=%d", event->aid);
     }
 }
 
+// Konfiguracja Access Point'a
 void wifi_init_softap(void)
 {
     ESP_ERROR_CHECK(esp_netif_init());
@@ -67,15 +69,14 @@ void wifi_init_softap(void)
 
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 
+    // Obsługa zdarzeń związanych z WiFi
     ESP_ERROR_CHECK(
         esp_event_handler_instance_register(
             WIFI_EVENT,
             ESP_EVENT_ANY_ID,
             &wifi_event_handler,
             NULL,
-            NULL
-        )
-    );
+            NULL));
 
     wifi_config_t wifi_config = {
         .ap = {
@@ -84,11 +85,11 @@ void wifi_init_softap(void)
             .channel = WIFI_CHANNEL,
             .password = WIFI_PASS,
             .max_connection = MAX_STA_CONN,
-            .authmode = WIFI_AUTH_WPA_WPA2_PSK
-        },
+            .authmode = WIFI_AUTH_WPA_WPA2_PSK},
     };
 
-    if (strlen(WIFI_PASS) == 0) {
+    if (strlen(WIFI_PASS) == 0)
+    {
         wifi_config.ap.authmode = WIFI_AUTH_OPEN;
     }
 
@@ -107,10 +108,11 @@ void wifi_init_softap(void)
 
 //------------------------------------ Koniec Access Point ---------------------------------------------------
 
+static esp_err_t ota_post_handler(httpd_req_t *req);
 //------------------------------------ Strona Internetowa ---------------------------------------------------
 
 extern const char index_html_start[] asm("_binary_index_html_start");
-extern const char index_html_end[]   asm("_binary_index_html_end");
+extern const char index_html_end[] asm("_binary_index_html_end");
 
 static esp_err_t root_get_handler(httpd_req_t *req)
 {
@@ -128,17 +130,26 @@ static httpd_handle_t start_webserver(void)
 
     httpd_config_t config = HTTPD_DEFAULT_CONFIG();
 
+    // Zwracanie strony
     static const httpd_uri_t root_uri = {
         .uri = "/",
         .method = HTTP_GET,
         .handler = root_get_handler,
-        .user_ctx = NULL
-    };
+        .user_ctx = NULL};
+
+    // Odbiór pliku firmware.bin
+    static const httpd_uri_t update_uri = {
+        .uri = "/update",
+        .method = HTTP_POST,
+        .handler = ota_post_handler,
+        .user_ctx = NULL};
 
     ESP_LOGI(TAG, "Starting HTTP server");
 
-    if (httpd_start(&server, &config) == ESP_OK) {
+    if (httpd_start(&server, &config) == ESP_OK)
+    {
         ESP_ERROR_CHECK(httpd_register_uri_handler(server, &root_uri));
+        ESP_ERROR_CHECK(httpd_register_uri_handler(server, &update_uri));
         ESP_LOGI(TAG, "HTTP server started");
         return server;
     }
@@ -147,18 +158,115 @@ static httpd_handle_t start_webserver(void)
     return NULL;
 }
 
-
 //------------------------------------ Koniec Strony Internetowej ---------------------------------------------------
+
+//------------------------------------  Obsługa update'ów ze stronki ---------------------------------------------------
+
+static esp_err_t ota_post_handler(httpd_req_t *req)
+{
+    // Feedback
+    esp_err_t ret;
+
+    // Szukanie partycji do update'u
+    const esp_partition_t *update_partition =
+        esp_ota_get_next_update_partition(NULL);
+
+    if (update_partition == NULL)
+    {
+        ESP_LOGE(TAG, "No OTA partition found");
+        return ESP_FAIL;
+    }
+
+    // Id aktualizacji
+    esp_ota_handle_t ota_handle;
+
+    // Nadpisanie nwoego firmware'a
+    ret = esp_ota_begin(
+        update_partition,
+        OTA_SIZE_UNKNOWN,
+        &ota_handle);
+
+    if (ret != ESP_OK)
+    {
+        ESP_LOGE(TAG, "OTA begin failed");
+        return ESP_FAIL;
+    }
+
+    // Temp memory
+    char buffer[1024];
+    int remaining = req->content_len;
+
+    // Paczkowe pobeiranie i zapisywanie danych
+    while (remaining > 0)
+    {
+
+        // Pobieranie danych ze stronki po 1KB
+        int received = httpd_req_recv(
+            req,
+            buffer,
+            sizeof(buffer));
+
+        if (received <= 0)
+        {
+            esp_ota_end(ota_handle);
+            return ESP_FAIL;
+        }
+
+        // Zapis do flash
+        ret = esp_ota_write(
+            ota_handle,
+            buffer,
+            received);
+
+        if (ret != ESP_OK)
+        {
+            esp_ota_end(ota_handle);
+            return ESP_FAIL;
+        }
+        remaining -= received;
+    }
+
+    ret = esp_ota_end(ota_handle);
+
+    if (ret != ESP_OK)
+    {
+        ESP_LOGE(TAG, "OTA end failed");
+        return ESP_FAIL;
+    }
+
+    // Ustawienie nowej default'owej partycji
+    ret = esp_ota_set_boot_partition(
+        update_partition);
+
+    if (ret != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Set boot partition failed");
+        return ESP_FAIL;
+    }
+
+    httpd_resp_sendstr(
+        req,
+        "OTA OK, restarting...");
+
+    vTaskDelay(pdMS_TO_TICKS(1000));
+
+    esp_restart();
+
+    return ESP_OK;
+}
+
+//------------------------------------ Koniec Obsługi Update'ów ---------------------------------------------------
 
 void app_main(void)
 {
 
     //--------------------------------- Access Point ----------------------------------------
 
-     esp_err_t ret = nvs_flash_init();
+    esp_err_t ret = nvs_flash_init();
 
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES ||
-        ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    {
         ESP_ERROR_CHECK(nvs_flash_erase());
         ret = nvs_flash_init();
     }
@@ -173,40 +281,15 @@ void app_main(void)
 
     //------------------------------------ Koniec Access Point ---------------------------------------------------
 
-    
+    //------------------------------------ Sprawdzenie aktualnej partycji ---------------------------------------------------
 
-    //------------------ Wyświetlanie informacji o ESP 32 s3 pico ---------------------------
+    const esp_partition_t *running = esp_ota_get_running_partition();
+    printf("Running partition: %s\n", running->label);
 
-    printf("Hello world!\n");
+    //------------------------------------ Koniec ---------------------------------------------------
 
-    /* Print chip information */
-    esp_chip_info_t chip_info;
-    uint32_t flash_size;
-    esp_chip_info(&chip_info);
-    printf("This is %s chip with %d CPU core(s), %s%s%s%s, ",
-           CONFIG_IDF_TARGET,
-           chip_info.cores,
-           (chip_info.features & CHIP_FEATURE_WIFI_BGN) ? "WiFi/" : "",
-           (chip_info.features & CHIP_FEATURE_BT) ? "BT" : "",
-           (chip_info.features & CHIP_FEATURE_BLE) ? "BLE" : "",
-           (chip_info.features & CHIP_FEATURE_IEEE802154) ? ", 802.15.4 (Zigbee/Thread)" : "");
-
-    unsigned major_rev = chip_info.revision / 100;
-    unsigned minor_rev = chip_info.revision % 100;
-    printf("silicon revision v%d.%d, ", major_rev, minor_rev);
-    if(esp_flash_get_size(NULL, &flash_size) != ESP_OK) {
-        printf("Get flash size failed");
-        return;
-    }
-
-    printf("%" PRIu32 "MB %s flash\n", flash_size / (uint32_t)(1024 * 1024),
-           (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
-
-    printf("Minimum free heap size: %" PRIu32 " bytes\n", esp_get_minimum_free_heap_size());
-
-    
-    while (1) {
+    while (1)
+    {
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
-
 }
