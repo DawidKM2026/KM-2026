e68cb8561da25602768a1a76ae9317a1a5933c88
dzikautomatyka <dzik.automatyka@gmail.com>
2026-07-13 10:14:45 +0200

    Zmieniono silnik IntelliSense na wyłączony oraz dodano funkcje do obsługi enkodera w pliku stepper_motor.h

--- hello_world/main/stepper_motor.h
+++ hello_world/main/stepper_motor.h
@@ -5,4 +5,11 @@
 void init_stepper_motor_timer(void);
 void motor_button_on_off(void);
 void motor_button_direction(void);
-void motor_set_speed(uint32_t motor_rpm);   
+void motor_set_speed(uint32_t motor_rpm);
+
+/* Encoder */
+
+void motor_encoder_init(void);
+int32_t motor_encoder_get_count(void);
+float motor_encoder_get_angle(void);
+void motor_encoder_reset_position(void);
