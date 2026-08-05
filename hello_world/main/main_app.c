979e4902a97fb7b31f8b308eb839c602472dd08c
dzikautomatyka <dzik.automatyka@gmail.com>
2026-08-05 10:04:43 +0200

    Add logging configuration to suppress log output in app_main

--- hello_world/main/main_app.c
+++ hello_world/main/main_app.c
@@ -1,10 +1,14 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "esp_log.h"
 
 #include "STATE/system_state.h"
 
 void app_main(void)
 {
+    // Ustawienie poziomu logowania na ESP_LOG_NONE, aby wyłączyć logi
+    esp_log_level_set("*", ESP_LOG_NONE);
+
     xTaskCreate(
         system_state_task,
         "system_state",
